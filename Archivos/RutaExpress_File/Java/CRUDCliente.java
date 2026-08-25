29f35ed2da7fb1d34d2c27152a74fc56b6f77c54
migman1983 <31773059+migman1983@users.noreply.github.com>
2026-08-25 07:51:27 -0500

    Add leerCliente method to CRUDCliente class
    Implemented the leerCliente method to read client data from a file and populate an array of Cliente objects.

--- Archivos/RutaExpress_File/Java/CRUDCliente.java
+++ Archivos/RutaExpress_File/Java/CRUDCliente.java
@@ -3,5 +3,40 @@ package RutaExpress;
 
 public class CRUDCliente 
 {
+  public Cliente[] leerCliente()
+    {
+        int cl = contadorLineas("DatosClientes.txt");
+        Cliente vecC[];
+        vecC = new Cliente[cl];
+        String registro;
+        String vec[];
+        vec = new String[3];
+       try
+        {
+           int i = 0;
+            while(cl>0)
+            {
+              registro = buffer.readLine();
+              String separador = Pattern.quote("|");
+               //  private String tipoDoc, numDoc,estadoCivil,lugarNacimiento,nombreAcudiente, telAcudiente;
+               //  private int estrato;
+              vec = registro.split(separador);
+               String cedula = vec[0];
+               String nombre = vec[1];
+               String telefono = vec[2];
+               Cliente objC = new Cliente (cedula,nombre,telefono);
+               vecC[i] = objC;
+               i++;
+               cl--;  
+            }
+         buffer.close();   
+        }
+        catch(Exception objException)
+        {
+         objException.getMessage();          
+        }
+        return vecC;
+    }
+   
     
 }
