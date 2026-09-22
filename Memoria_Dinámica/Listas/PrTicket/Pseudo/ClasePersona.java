8eaf2b6238bac139df9a8d3bfc62bf335e92cb2f
AndresVegaP17 <andresvega1168798@correo.itm.edu.co>
2026-09-22 06:46:03 -0500

    Crear ClasePersona.java

---
+++ Memoria_Dinámica/Listas/PrTicket/Pseudo/ClasePersona.java
@@ -0,0 +1,49 @@
+public class Persona {
+
+    private int id;
+    private String tipoDoc;
+    private String nombre;
+    private char idServicio;
+    private String ticketServicio;
+
+    public Persona(int id, String tipoDoc, String nombre,
+                   char idServicio, String ticketServicio) {
+
+        this.id = id;
+        this.tipoDoc = tipoDoc;
+        this.nombre = nombre;
+        this.idServicio = idServicio;
+        this.ticketServicio = ticketServicio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTipoDoc() {
+        return tipoDoc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public char getIdServicio() {
+        return idServicio;
+    }
+
+    public String getTicketServicio() {
+        return ticketServicio;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", tipoDoc='" + tipoDoc + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", idServicio=" + idServicio +
+                ", ticketServicio='" + ticketServicio + '\'' +
+                '}';
+    }
+}
